5ee8656750ae9b52cf68e31ed8d02875e903aff4
tranthanhhthao <90049000+tranthanhhthao@users.noreply.github.com>
2024-01-28 23:39:55 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ const scores = document.getElementsByClassName('score');
 const totals = document.getElementsByClassName('total');
 const percentages = document.getElementsByClassName('percentage');
 
-const output = document.getElementById('output');
-const button = document.getElementById('submit');
+const buttonSubmit = document.getElementById('submit');
 
+const classGrade = document.getElementById('output_grade');
 const classScore = document.getElementById('output_score');
+const classCurrentSum = document.getElementById('output_currentSum');
+const classCurrentTotal = document.getElementById('output_currentTotal')
 
-button.addEventListener('click', handleClick)
+buttonSubmit.addEventListener('click', handleClick())
 
 function handleClick() {
     let sumScore = 0;
-    let sumTotal = 0;
+    let sumCurrent = 0;
+    let currentTotal = 0;
     let count = 0;
     let average;
 
@@ -20,25 +23,35 @@ function handleClick() {
 
         if (scores[i].value !== '' && totals[i].value !== '') {
             sumScore += Number(scores[i].value)
-            sumTotal += Number(totals[i].value)
+            sumCurrent += Number(totals[i].value)
             count++
+
+            classScore.innerHTML = sumScore;
+            classCurrentSum.innerHTML = sumCurrent
+        }
+
+        if (totals[i].value !== '') {
+            currentTotal += Number(totals[i].value)
+
+            classCurrentTotal.innerHTML = currentTotal
         }
  
-        average = (sumScore/sumTotal)* 100;
+        average = (sumScore/sumCurrent)* 100;
     }
 
-    output.innerHTML = average.toFixed(2) + '%';
+    classGrade.innerHTML = average.toFixed(2) + '%';
 
 
     // percentage
     for (let i = 0; i < scores.length; i++) {
 
-        if (totals[i].value !== '') {
+        if (totals[i].value !== '' && scores[i].value !== '') {
             percentages[i].innerHTML = Math.round((Number(scores[i].value) / Number(totals[i].value) * 100)*100)/100 + '%'
         }
     
     }
 
     // output
-    classScore.innerHTML = average.toFixed(2) + '%'
+    classGrade.innerHTML = average.toFixed(2) + '%'
+
 }
