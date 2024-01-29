96837ab2f0037fa13063e1d9f2ed7c3c89445f59
tranthanhhthao <90049000+tranthanhhthao@users.noreply.github.com>
2024-01-29 16:10:27 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const inputs = document.getElementById('frm1');
+const asm = document.getElementsByClassName('assignment');
 const scores = document.getElementsByClassName('score');
 const totals = document.getElementsByClassName('total');
 const percentages = document.getElementsByClassName('percentage');
 
+const pointer = document.getElementById('pointer');
 const buttonSubmit = document.getElementById('submit');
+const buttonClear = document.getElementById('clear');
 
 const classGrade = document.getElementById('output_grade');
 const classScore = document.getElementById('output_score');
 const classCurrentSum = document.getElementById('output_currentSum');
-const classCurrentTotal = document.getElementById('output_currentTotal')
+const classCurrentTotal = document.getElementById('output_currentTotal');
 
-buttonSubmit.addEventListener('click', handleClick())
 
-function handleClick() {
+function reload() {
     let sumScore = 0;
     let sumCurrent = 0;
     let currentTotal = 0;
@@ -28,30 +30,64 @@ function handleClick() {
 
             classScore.innerHTML = sumScore;
             classCurrentSum.innerHTML = sumCurrent
-        }
+        } 
 
         if (totals[i].value !== '') {
             currentTotal += Number(totals[i].value)
-
             classCurrentTotal.innerHTML = currentTotal
-        }
+        } 
  
         average = (sumScore/sumCurrent)* 100;
     }
-
-    classGrade.innerHTML = average.toFixed(2) + '%';
-
 
     // percentage
     for (let i = 0; i < scores.length; i++) {
 
         if (totals[i].value !== '' && scores[i].value !== '') {
             percentages[i].innerHTML = Math.round((Number(scores[i].value) / Number(totals[i].value) * 100)*100)/100 + '%'
+        } else if (totals[i].value === '' || scores[i].value === '') {
+            percentages[i].innerHTML = ''
         }
     
     }
 
     // output
-    classGrade.innerHTML = average.toFixed(2) + '%'
+    classGrade.innerHTML = average.toFixed(2) + '%';
+    pointer.style.opacity = 1
+    pointer.style.left = 2.35 * average + 'px'
+    
+}
+
+reload()
+
+function clearing() {
+    
+    for (let i = 0; i < inputs.length; i++) {
+        // inputs.elements[i].value = 0;
+        inputs.elements[i].value = ''
+    }
+
+
+    reload()
+    pointer.style.opacity = 0;
+
+    classScore.innerHTML = '';
+    classGrade.innerHTML = '';
+    classCurrentSum.innerHTML = '';
+    classCurrentTotal.innerHTML = '';
 
 }
+
+function example() {
+    asm[2].value = '245'
+    asm[3].value = 'Draft 1';
+    asm[4].value = 'Draft 2';
+    asm[5].value = 'Peer Review';
+    asm[6].value = 'Writing Project 1';
+
+    scores[3].value = 0;
+    totals[3].value = 30;
+
+    reload()
+}
+
